69943237339f8c85af011a85e87c0f96948cbe6a
Aylin Yıldız <aylinyildiz1628@icloud.com>
2021-03-01 11:39:11 +0300

    Create route-generator.js

---
+++ route-generator.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+const rota = [
+  {
+    source: "JPN",
+    destination: "PHL"
+  },
+  {
+    source: "BRA",
+    destination: "UAE"
+  },
+  {
+    source: "USA",
+    destination: "BRA"
+  },
+  {
+    source: "UAE",
+    destination: "JPN"
+  }
+];
+
+function sirala(rota) {
+  let begin = rota[0]["source"];
+  let newArr = [];
+  let v = 0;
+
+  for (let i = 0; i < rota.length; i++) {
+    if (rota[i]["destination"] === begin) {
+      begin = rota[i]["source"];
+      while (v < rota.length) {
+        if (begin === rota[v]["destination"]) {
+          begin = rota[v]["source"];
+          v = 0;
+        } else {
+          v += 1;
+        }
+      }
+    }
+  }
+  v = 0;
+  for (let i = 0; i < rota.length; i++) {
+    if (rota[i]["source"] === begin) {
+      //bra
+      newArr.push(begin);
+      begin = rota[i]["destination"];
+      newArr.push(begin);
+      while (v < rota.length) {
+        if (begin === rota[v]["source"]) {
+          begin = rota[v]["destination"];
+          newArr.push(begin);
+          v = 0;
+        } else {
+          v += 1;
+        }
+      }
+    }
+  }
+  return newArr;
+}
+
+console.log(sirala(rota));
+
+document.getElementById("app").innerHTML = `
+<h3>${sirala(rota)}</h3> `;
