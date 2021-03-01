91ef430878b797849afcde4b4886774add194c50
Aylin Yıldız <aylinyildiz1628@icloud.com>
2021-03-01 11:47:57 +0300

    Update route-generator.js

--- route-generator.js
+++ route-generator.js
@@ -18,46 +18,42 @@ const rota = [
   }
 ];
 
-function sirala(rota) {
-  let begin = rota[0]["source"];
-  let newArr = [];
-  let v = 0;
+let begin = rota[0]["source"];
+let newArr = [];
+let v = 0;
 
-  for (let i = 0; i < rota.length; i++) {
-    if (rota[i]["destination"] === begin) {
-      begin = rota[i]["source"];
-      while (v < rota.length) {
-        if (begin === rota[v]["destination"]) {
-          begin = rota[v]["source"];
-          v = 0;
-        } else {
-          v += 1;
-        }
+for (let i = 0; i < rota.length; i++) {
+  if (rota[i]["destination"] === begin) {
+    begin = rota[i]["source"];
+    while (v < rota.length) {
+      if (begin === rota[v]["destination"]) {
+        begin = rota[v]["source"];
+        v = 0;
+      } else {
+        v += 1;
       }
     }
   }
-  v = 0;
-  for (let i = 0; i < rota.length; i++) {
-    if (rota[i]["source"] === begin) {
-      //bra
-      newArr.push(begin);
-      begin = rota[i]["destination"];
-      newArr.push(begin);
-      while (v < rota.length) {
-        if (begin === rota[v]["source"]) {
-          begin = rota[v]["destination"];
-          newArr.push(begin);
-          v = 0;
-        } else {
-          v += 1;
-        }
+}
+v = 0;
+for (let i = 0; i < rota.length; i++) {
+  if (rota[i]["source"] === begin) {
+    //bra
+    newArr.push(begin);
+    begin = rota[i]["destination"];
+    newArr.push(begin);
+    while (v < rota.length) {
+      if (begin === rota[v]["source"]) {
+        begin = rota[v]["destination"];
+        newArr.push(begin);
+        v = 0;
+      } else {
+        v += 1;
       }
     }
   }
-  return newArr;
 }
 
-console.log(sirala(rota));
+console.log(newArr);
 
-document.getElementById("app").innerHTML = `
-<h3>${sirala(rota)}</h3> `;
+document.getElementById("app").innerHTML = ` `;
